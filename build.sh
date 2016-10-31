c8271db0180d42661fc2aa1cef94050aa69c1f6a
Phil Howard <phil@gadgetoid.com>
2016-10-31 16:25:10 +0000

    Fix for #6

--- build.sh
+++ build.sh
@@ -4,8 +4,22 @@ echo "Generating bindings"
 swig2.0 -perl wiringpi.i
 
 CORE=`perl -MConfig -e 'print $Config{archlib}'`/CORE
+WIRINGPI=WiringPi/wiringPi
 
 echo "Building against: $CORE"
 
-gcc -fpic -c -Dbool=char -I$CORE wiringpi_wrap.c WiringPi/wiringPi/wiringPi.c -D_GNU_SOURCE
-gcc -shared wiringPi.o wiringpi_wrap.o -o wiringpi.so
+gcc -fpic -c -Dbool=char -I$CORE wiringpi_wrap.c \
+$WIRINGPI/wiringSerial.c \
+$WIRINGPI/wiringShift.c \
+$WIRINGPI/wiringPi.c \
+$WIRINGPI/softPwm.c \
+$WIRINGPI/softTone.c \
+-D_GNU_SOURCE
+
+gcc -shared wiringPi.o \
+softPwm.o \
+softTone.o \
+wiringSerial.o \
+wiringShift.o \
+wiringpi_wrap.o \
+-o wiringpi.so
