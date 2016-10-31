47a85d9d4097e6132a4e51191a11dec3e625af20
Phil Howard <phil@gadgetoid.com>
2016-10-31 16:12:38 +0000

    Fixed build

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
-gcc -fpic -c -Dbool=char -I/usr/lib/perl/$(perl -v | grep \(v | cut -d "(" -f 2 | cut -d ")" -f 1 | sed -e 's/v//')/CORE wiringpi_wrap.c WiringPi/wiringPi/wiringPi.c -D_GNU_SOURCE
+#!/bin/bash
+
+echo "Generating bindings"
+swig2.0 -perl wiringpi.i
+
+CORE=`perl -MConfig -e 'print $Config{archlib}'`/CORE
+
+echo "Building against: $CORE"
+
+gcc -fpic -c -Dbool=char -I$CORE wiringpi_wrap.c WiringPi/wiringPi/wiringPi.c -D_GNU_SOURCE
 gcc -shared wiringPi.o wiringpi_wrap.o -o wiringpi.so
